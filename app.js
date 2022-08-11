2c1c2d7befdcae63b0e1f33a01994c057054c674
Nguyễn Xuân Bách <110024359+Yukimarin@users.noreply.github.com>
2022-08-11 11:25:55 +0700

    Homework6.1

---
+++ app.js
@@ -0,0 +1,78 @@
+// Bài 1: Viết chương trình cộng hai số
+// function tong() {
+//     let a = Number(prompt('Nhập vào số a'));
+//     let b = Number(prompt('Nhập vào số b'));
+//     return a + b;
+// }
+// console.log(tong());
+
+//Bai 2: Tính tổng số lẻ trong mảng
+// array = [1,2,3,4,5,6]
+// function tongsole(array) {
+//     let tong = 0
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] %2 !=0){
+//             tong = tong + array[i];
+//         }    
+//     }
+//     return tong
+// }
+// console.log(tongsole(array));
+
+// Bài 3: 
+// function sosanh2so() {
+//     let a = Number(prompt('Nhập vào số a bất kì'));
+//     let b = Number(prompt('Nhập vào số b bất kì'));
+//     if (a < b){
+//         console.log ('Số a nhập vào nhỏ hơn số b');
+//     } else {
+//         return a+b;
+//     }
+// }
+// console.log(sosanh2so());
+
+// Bài 4: 
+// function addNumbers() {
+//     firstNum = 4;
+//     seconNum = 8; 
+//     result = firstNum + seconNum; 
+//     alert(0)
+//     return result
+// }
+// alert(addNumbers())
+
+// Bài 5: 
+// function footToMeter() {
+//     let foot = Number(prompt('Nhập vào số feet cần chuyển đổi'));
+//     return meter = 0.305*foot
+// }
+// console.log(footToMeter());
+
+// function meterToFoot() {
+//     let meter = Number(prompt('Nhập vào số meter cần chuyển đổi'));
+//     return foot = 3.279*meter
+// }
+// console.log(meterToFoot());
+
+// Bài 6: 
+// let a = Number(prompt("Nhap vao so a"))
+// let check = 0
+// function kiemTraSoNguyenTo(a) {
+//     if (a === 2) {
+//         alert("a là số nguyên tố")
+//         return
+//     } else {
+//         for (let i = 2; i < a; i++) {
+//             if (a % i === 0) {
+//                 alert("a không phải là số nguyên tố")
+//                 check = 1
+//                 break;
+//             }
+//         } if (check === 0) {
+//             alert("A là số nguyên tố")
+//             return
+//         }
+//     }
+// } kiemTraSoNguyenTo(a)
+
+
